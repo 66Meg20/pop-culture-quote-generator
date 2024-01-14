d5eca8b1bca44852ae7c90b7d32ae30259889485
66Meg20 <66meg.dj2066@gmail.com>
2024-01-13 18:10:44 -0600

    added the basic javascript and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+function generateQuote(event) {
+  event.preventDefault();
+  new Typewriter("#quote", {
+    strings: "Light up the darkness majesty",
+    autoStart: true,
+  });
+}
 
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
