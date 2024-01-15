d169df0f600e855b445d3e752a83edc58d992629
66Meg20 <66meg.dj2066@gmail.com>
2024-01-14 21:14:52 -0600

    changed the content for the context

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ function generateQuote(event) {
   let apiKey = "a1184840edbfda3e40off6tdfda47219";
   let instructionsInput = document.querySelector("#user-instructions");
   let context =
-    "You are a pop culture expert, and can come up with a quote from anything be it book or movie or television show. your mission is to generate a new quote every time 5 lines max. Please make sure to follow the user instructions.  please also include the original person who quoted the quote. only quote quotes from the book television show or movie, not quotes about them";
-  let prompt = `user instructions: Please generate a pop culture referencing quote based on the ${instructionsInput.value}`;
+    "You are a pop culture expert, and can remember quotes from anything be it book or movie or television show. your mission is to generate a new quote every time. Please make sure to follow the user instructions. only quote characters from the book television show or movie.";
+  let prompt = `user instructions: Please generate a quote based on the ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating poem");
