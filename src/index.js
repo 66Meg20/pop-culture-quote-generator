a682437d33afaf0745f16a91d9762f1b8287b174
66Meg20 <66meg.dj2066@gmail.com>
2024-01-15 00:45:01 -0600

    improved the user experience with css classes and javascript

--- src/index.js
+++ src/index.js
@@ -14,10 +14,9 @@ function generateQuote(event) {
     "You are a pop culture expert, and can remember quotes from anything be it book or movie or television show. your mission is to generate a new quote every time. Please make sure to follow the user instructions. only quote characters from the book television show or movie.";
   let prompt = `user instructions: Please generate a quote from ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("generating poem");
-  console.log(`context ${context}`);
-  console.log(`prompt: ${prompt}`);
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="generating">⏳Generating a quote about ${instructionsInput.value} </div>`;
 
   axios.get(apiUrl).then(displayQuote);
 }
